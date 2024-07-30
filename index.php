3b3b46c97320c59c69cbbc0b9a51f184e8b208d0
Axel Westman <111924075+AxelWestman@users.noreply.github.com>
2024-07-30 20:40:09 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -12,12 +12,29 @@ $data = json_decode($result, true);
 
 curl_close($ch);
 
-var_dump($data);
-
 ?>
 
+<head>
+    <meta charset="UTF-8" />
+    <title>La próxima película de Marvel</title>
+    <meta name="description" content="La próxima película de Marvel" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
+    />
+</head>
+
 <main>
-    <h2>La próxima película de Marvel</h2>
+    <section>
+        <img src="<?= $data["poster_url"]; ?>" width="300" alt="Poster de <?= $data["title"]; ?>" style="border-radius: 16px" />
+    </section>
+    <hgroup>
+        <h3><?= $data["title"]; ?> Se estrena en <?= $data["days_until"]; ?> días</h3>
+        <p>Fecha de estreno <?= $data["release_date"];  ?></p>
+        <p>La siguiente es: <?= $data["following_production"]["title"]; ?> </p>
+    </hgroup>
+    
 </main>
 
 <style>
